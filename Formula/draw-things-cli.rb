b87fda986bafc12ed1bde2cd347920edf1ae2c01
fkzwym <fkzwym@gmail.com>
2026-03-04 20:44:15 +0900

    Update draw-things-cli to v1.20260303.0

--- Formula/draw-things-cli.rb
+++ Formula/draw-things-cli.rb
@@ -1,9 +1,9 @@
 class DrawThingsCli < Formula
   desc "Draw Things gRPC Server CLI"
   homepage "https://github.com/drawthingsai/draw-things-community"
-  version "v1.20260209.0"
-  url "https://github.com/drawthingsai/draw-things-community/releases/download/v1.20260209.0/gRPCServerCLI-macOS"
-  sha256 "22a3e731503c63aceeae4b0fb895c6ba136e03323397709fdef76ba488dc7b4f"
+  version "v1.20260303.0"
+  url "https://github.com/drawthingsai/draw-things-community/releases/download/v1.20260303.0/gRPCServerCLI-macOS"
+  sha256 "1e734037d579447ad5dde24acdb18c9fd1f435568d02311e3c1dad2589d1df69"
 
   def install
     bin.install "gRPCServerCLI-macOS" => "draw-things-gRPCServerCLI"
